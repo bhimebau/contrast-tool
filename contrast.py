aed62aa2cb25ba9a76b8fcb9de2ed5d3f2ae501b
Bryce Himebaugh <bhimebau@gmail.com>
2014-04-17 12:23:06 -0400

    aligned repository with the current version used in the Begley lab

--- contrast.py
+++ contrast.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 
 __author__ = "Bryce Himebaugh"
 __copyright__ = "Copyright 2013 Bryce Himebaugh"
@@ -10,12 +10,15 @@ __email__ = "bhimebau@gmail.com"
 __status__ = "prototype"
 
 import pygame, sys, getopt, time
+import math
 
 imagefile = ''
 prefix = 'cdata'
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"hi:o:",["image=","prefix="])
+    opts, args = getopt.getopt(sys.argv[1:],"hi:o:p:i:",["image=","prefix="])
 except getopt.GetoptError:
+    print opts
+    print args
     print 'contrast.py <target image file> -p <output_datafile_prefix>'
     sys.exit(2)
 for opt, arg in opts:
@@ -40,13 +43,17 @@ window=pygame.display.set_mode((0, 0),pygame.RESIZABLE)
 pygame.display.set_caption("Contrast Tool")
 center=(window.get_rect().width/2,window.get_rect().height/2)
 background=pygame.Surface((window.get_rect().width, window.get_rect().height))
-background.fill((0, 0, 0))
+#background.fill((0, 0, 0))
+background.fill((255, 255, 255))
 image=pygame.image.load(imagefile)
 image=image.convert()
-center_image = (image.get_rect().width/2,image.get_rect().height/2)
-image_loc = (center[0]-center_image[1],center[1]-center_image[1])
+center_image = (image.get_rect().width/4,image.get_rect().height/4)
+image_loc = (center[0]-center_image[0],center[1]-center_image[1])
 contrast_value=0
-
+mm=contrast_value/255;
+#y=1.6973*(mm**3) - 1.7375*(mm**2) +1.0494*mm - 0.0286;
+y=1.6973*(mm**3) - 1.7375*(mm**2) +1.0494*mm;
+contrast_value= contrast_value+ float(255*y);
 while True:
     # Manage quit and window resize events
     for event in pygame.event.get():
@@ -56,17 +63,28 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             print "key event", event.key
+            out = "%d            %d\n"%(pygame.time.get_ticks(),event.key)
+            df.write(out)
             if event.key == pygame.K_q:
-                sys.exit()
+                 sys.exit()
         if event.type == pygame.VIDEORESIZE:
             size = event.size
             center = (size[0]/2,size[1]/2)
-            image_loc = (center[0]-center_image[1],center[1]-center_image[1])
+            image_loc = (center[0]-center_image[0],center[1]-center_image[1])
             print center,center_image,image_loc
 
     # Grab the change in the y position of the mouse, apply this to the current contrast value.
     mdelta = pygame.mouse.get_rel()
-    contrast_value -= mdelta[1]/2;
+    #print (mdelta[0])
+    #contrast_value  =math.log10(contrast_value);
+    #print (contrast_value);
+    m=float(-mdelta[0]);
+    #print (m);
+    #mm=(contrast_value+m)/255;
+    contrast_value -= m/2;
+    #print (contrast_value);
+    #contrast_value  =math.pow(10, contrast_value);
+    #print (contrast_value);
 
     # Clamp the value to put it on the alpha channel scale
     if contrast_value < 0:
@@ -74,18 +92,20 @@ while True:
     elif contrast_value > 255:
         contrast_value = 255;
 
-    if mdelta > 0:
+    if mdelta > 0: 
         outstr = "%d %d\n"%(pygame.time.get_ticks(),contrast_value)
         df.write(outstr)
 
     # write to the alpha channel
     image.set_alpha(contrast_value)
-
+    
+   
+    
     # sent the new surface to the screen
     window.fill((255, 255, 255))
     window.blit(background, background.get_rect())
     window.blit(image, image_loc)
     pygame.time.delay(20)
     pygame.display.update()
-
+    print (contrast_value);
 
